62c9d363835bd89bec76f7936bff1f8b3321c1d3
Tweber663 <tweber.1995@hotmail.com>
2024-09-12 14:48:10 +0200

    JS/TS added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,13 @@
-import Stopwatch from './StopwatchWithResults.js';
-new Stopwatch(document.querySelector('.stopwatch'));
+"use strict";
+const splits = document.querySelectorAll('.split');
+const container = document.querySelector('.container');
+Array.from(splits).forEach((split) => {
+    split.addEventListener('mouseenter', (s) => {
+        const target = s.target;
+        const element = target.classList[1];
+        element === 'right' ? container.classList.add('hover-right') : container.classList.remove('hover-right');
+        element === 'left' ? container.classList.add('hover-left') : container.classList.remove('hover-left');
+    });
+    split.addEventListener('mouseleave', (e) => container.classList.remove(container.classList[1]));
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import Stopwatch from './StopwatchWithResults.js'
+const splits = document.querySelectorAll('.split');
+const container = document.querySelector('.container') as HTMLDivElement; 
 
-new Stopwatch(document.querySelector('.stopwatch'))
+
+Array.from(splits).forEach((split) => {
+    split.addEventListener('mouseenter', (s) => {
+        const target = s.target as HTMLDivElement; 
+        const element = target.classList[1]; 
+        element === 'right'? container.classList.add('hover-right') : container.classList.remove('hover-right')
+        element === 'left'? container.classList.add('hover-left') : container.classList.remove('hover-left')
+    })
+    split.addEventListener('mouseleave', (e) => container.classList.remove(container.classList[1])); 
+    })
